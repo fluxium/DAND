aa6e38c37fdd67ed75afe81e26fef3d1728a3b4c
Mahlon Barrault <mahlon@live.ca>
2015-08-12 07:13:40 -0600

    Commit yesterday did not get all of the files. Adding this rest this morning. Git is reporting a change to Excel_To_CSV.py but I am not sure what that change was.

--- Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Lesson 1/ps/Excel_To_CSV.py
+++ Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Lesson 1/ps/Excel_To_CSV.py
@@ -9,7 +9,9 @@ import os
 import csv
 from zipfile import ZipFile
 
-datafile = "2013_ERCOT_Hourly_Load_Data.xls"
+import numpy as np
+
+datafile = "../datasets/2013_ERCOT_Hourly_Load_Data.xls"
 outfile = "2013_Max_Loads.csv"
 
 
@@ -17,22 +19,64 @@ def open_zip(datafile):
     with ZipFile('{0}.zip'.format(datafile), 'r') as myzip:
         myzip.extractall()
 
+def parse_file_instructor(datafile): # Instructor Code
+    workbook = xlrd.open_workbook(datafile)
+    sheet = workbook.sheet_by_index(0)
+    data = {}
+    # process all rows that contain station data
+    for n in range (1, 9):
+        station = sheet.cell_value(0, n)
+        cv = sheet.col_values(n, start_rowx=1, end_rowx=None)
+
+        maxval = max(cv)
+        maxpos = cv.index(maxval) + 1
+        maxtime = sheet.cell_value(maxpos, 0)
+        realtime = xlrd.xldate_as_tuple(maxtime, 0)
+        data[station] = {"maxval": maxval,
+                         "maxtime": realtime}
+
+    print data
+    return data
+
+def save_file_instructor(data, filename): # Instructor Code
+    with open(filename, "w") as f:
+        w = csv.writer(f, delimiter='|')
+        w.writerow(["Station", "Year", "Month", "Day", "Hour", "Max Load"])
+        for s in data:
+            year, month, day, hour, _ , _= data[s]["maxtime"]
+            w.writerow([s, year, month, day, hour, data[s]["maxval"]])
 
 def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
     sheet = workbook.sheet_by_index(0)
-    data = None
+    data = []
     # YOUR CODE HERE
     # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
     # Excel date to Python tuple of (year, month, day, hour, minute, second)
+    dates = np.array(sheet.col_values(0)[1:])
+    
+    for col in range(1, sheet.ncols - 1):
+        station = sheet.cell_value(0, col)
+        col_array = np.array(sheet.col_values(col)[1:])
+        col_max_idx = col_array.argmax()
+        col_max = col_array[col_max_idx]
+        max_time = xlrd.xldate_as_tuple(dates[col_max_idx], 0)
+        
+        col_summary = [station, max_time[0], max_time[1], max_time[2], max_time[3], col_max]
+        data.append(col_summary)
+        
     return data
 
 def save_file(data, filename):
     # YOUR CODE HERE
-
+    header = ['Station', 'Year', 'Month', 'Day', 'Hour', 'Max Load']
+    with open(filename, 'wb') as f:
+        writer = csv.writer(f, delimiter='|')
+        writer.writerow(header)        
+        writer.writerows(data)
     
 def test():
-    open_zip(datafile)
+    #open_zip(datafile)
     data = parse_file(datafile)
     save_file(data, outfile)
 
