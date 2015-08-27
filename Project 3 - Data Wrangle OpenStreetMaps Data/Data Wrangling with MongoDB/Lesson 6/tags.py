bc55739d4aa29ddeccb4cb097a9252ae935f90c8
Mahlon Barrault <mahlonbarrault@gmail.com>
2015-08-27 14:37:16 -0600

    Changing example.osm back to the once associated with the tag and user exercises. tags.py tested and meets spec.

--- Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Lesson 6/tags.py
+++ Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Lesson 6/tags.py
@@ -26,7 +26,14 @@ problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 def key_type(element, keys):
     if element.tag == "tag":
         # YOUR CODE HERE
-        pass
+        if lower_colon.search(element.attrib['k']):
+            keys['lower_colon'] += 1
+        elif problemchars.search(element.attrib['k']):
+            keys['problemchars'] += 1
+        elif lower.search(element.attrib['k']):
+            keys['lower'] += 1
+        else:
+            keys['other'] += 1
         
     return keys
 
@@ -36,7 +43,6 @@ def process_map(filename):
     keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
     for _, element in ET.iterparse(filename):
         keys = key_type(element, keys)
-
     return keys
 
 
