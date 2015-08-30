b8f3f3b81128b6072d88d66a4851f40a26b1124b
Mahlon Barrault <mahlonbarrault@gmail.com>
2015-08-30 13:47:05 -0600

    Finished copying functions from data.py to clean.py.

--- Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Project/clean.py
+++ Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Project/clean.py
@@ -12,8 +12,16 @@ import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
 import pprint
+import codecs
+import json
 
 OSMFILE = "calgary_canada.osm"
+
+# RegEx for various key validations
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
 # These values were collectect from audit.py. They are the values that were not
@@ -55,6 +63,7 @@ ST_TYPE_EXPECTED = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place",
             
 DIR_EXPECTED = ['N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SW']
 
+#CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
 
 # Clean Street Names
 
@@ -65,3 +74,108 @@ DIR_EXPECTED = ['N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SW']
 #   check for street type in street name with suffix removed
 #   clean street type
 # append suffix to cleaned street name
+
+
+def shape_base(element):
+    
+    node = defaultdict()    
+    
+    created_keys = ["version", "changeset", "timestamp", "user", "uid"]
+    root_keys = ['id', 'visible']    
+    
+    node['type'] = element.tag
+    node['created'] = defaultdict()
+    
+    # Probably could use the .get() method on element here
+    if 'lat' in element.attrib and 'lon' in element.attrib:
+        node['pos'] = [float(element.attrib['lat']), float(element.attrib['lon'])]       
+
+    # http://stackoverflow.com/questions/3294889/iterating-over-dictionaries-using-for-loops-in-python    
+    for k, v in (element.attrib).iteritems():
+        if k in created_keys:
+            node['created'][k] = v
+        elif k in root_keys:
+            node[k] = v
+            
+    return shape_node(node, element)
+    
+
+def regex_key(k):
+    l = lower.search(k)
+    lc = lower_colon.search(k)
+    pc = problemchars.search(k)
+
+    return l, lc, pc
+
+
+def shape_node(node, element):
+    
+    node_refs = []
+    address = defaultdict()    
+    
+    for t in element:
+        k = t.attrib.get('k')
+        v = t.attrib.get('v')
+        r = t.attrib.get('ref')
+        
+        if k:
+            l, lc, pc = regex_key(k)       
+            if pc == None:
+                if k.startswith('addr'):
+                    if lc:
+                        # FIXME The v in this line need to be run through data 
+                        # cleaning for street types
+                        address[k.split(':')[1]] = v
+                    else:
+                        continue
+                else:
+                    node[k] = v
+        if r:
+            node_refs.append(r)
+            
+        if len(address) > 0:
+            node['address'] = address
+        if len(node_refs) > 0:
+            node['node_refs'] = node_refs
+    return node
+
+
+def shape_element(element):
+    node = {}
+    
+    if element.tag == "node" or element.tag == "way" :
+        # YOUR CODE HERE
+        node = shape_base(element)
+        
+        return dict(node)
+    else:
+        return None
+
+
+# http://stackoverflow.com/questions/3543559/python-regex-match-and-replace
+def process_match(m):
+    if ST_TYPE_MAPPING.get(m.group()) != None:
+        return ST_TYPE_MAPPING.get(m.group())
+    else:
+        return m.group()
+    return 
+
+
+def update_name(name): 
+    return street_type_re.sub(process_match, name)
+    
+# Instructor Code
+def process_map(file_in, pretty = False):
+    # You do not need to change this file
+    file_out = "{0}.json".format(file_in)
+    data = []
+    with codecs.open(file_out, "w") as fo:
+        for _, element in ET.iterparse(file_in):
+            el = shape_element(element)
+            if el:
+                data.append(el)
+                if pretty:
+                    fo.write(json.dumps(el, indent=2)+"\n")
+                else:
+                    fo.write(json.dumps(el) + "\n")
+    return data
