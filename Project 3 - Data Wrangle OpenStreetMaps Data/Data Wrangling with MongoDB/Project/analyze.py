7e75c473c75db38cf23b7d597af63195011e17d8
Mahlon Barrault <mahlonbarrault@gmail.com>
2015-08-31 17:39:33 -0600

    Committing work on IPython notebook and updates to the HTML export.

--- Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Project/analyze.py
+++ Project 3 - Data Wrangle OpenStreetMaps Data/Data Wrangling with MongoDB/Project/analyze.py
@@ -25,14 +25,25 @@ def make_pipeline():
     return pipeline
 
 def get_all_docs(db):
-    return db.calgary_canada_osm.find({'address.street' : {'$exists' : 1}})
+    return db.calgary_canada_osm.find()
 
 # Instructor Code
 def aggregate(db, pipeline):
     result = db.calgary_canada_osm.aggregate(pipeline)
     return result
 
+def get_largest_doc(results):
+    max_doc_len = 0
+    max_doc = {}
+    for r in results:
+        current_len = len(str(r))
+        if current_len > max_doc_len:
+            max_doc_len = current_len
+            max_doc = r
+    return (max_doc_len, max_doc)
 
 db = get_db('osm')
 #pipeline = make_pipeline()
 #result = aggregate(db, pipeline)
+#print db.calgary_canada_osm.count()
+print get_largest_doc(get_all_docs(db))
